6f058431b9726c4366cbf167a820f0551b27377d
Alexey <butuzz74@gmail.com>
2025-03-12 13:40:03 +0300

    fixed mistakes build

--- app/api/projector/route.ts
+++ app/api/projector/route.ts
@@ -4,14 +4,14 @@ import { NextResponse } from 'next/server';
 
 export async function GET() {
   try {
-    console.log('🔄 Обрабатываем GET-запрос...');
+    // console.log('🔄 Обрабатываем GET-запрос...');
     await connectDB();
-    console.log('🔄 Ищем все проекторы в БД...');
-    const projectors = await Projector.find().select("-__v -createdAt -updatedAt");
-    console.log('✅ Данные успешно получены:', projectors);
-    return NextResponse.json(projectors);
+    // console.log('🔄 Ищем все проекторы в БД...');
+    const projectors = await Projector.find().select("-__v -createdAt -updated");
+    // console.log('✅ Данные успешно получены:', projectors);    
+    return NextResponse.json(projectors);    
   } catch (error) {
-    console.error('❌ Ошибка в API route /api/projector:', error);
+    // console.error('❌ Ошибка в API route /api/projector:', error);
     return NextResponse.json({ message: 'Ошибка сервера' }, { status: 500 });
   }
 }
--- app/projectors/page.tsx
+++ app/projectors/page.tsx
@@ -7,16 +7,15 @@ export const metadata: Metadata = {
 };
 
 async function ProjectorsAll() {
-  // const data = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/projector`)
-  // const projectors : ProjectorType[] = await data.json()  
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/projector`);
-const text = await res.text();
-console.log('API Response:', text);
-const data = JSON.parse(text);
+  const data = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/projector`,
+    { cache: 'no-store' }
+  );
+  const projectors: ProjectorType[] = await data.json();
+
   return (
-    <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">  
-    <h1 className='text-black'>ojhlkjkj</h1>    
-      {/* {projectors.map((projector) => (
+    <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+      {projectors.map((projector) => (
         <ProjectorCard
           key={projector._id}
           id={projector._id}
@@ -28,7 +27,7 @@ const data = JSON.parse(text);
           access={projector.access}
           description={projector.description}
         />
-      ))} */}
+      ))}
     </div>
   );
 }
